8723ddd6c0bece29055459cc0528b10a8b362c82
Fazel HosseinzadehDastak <fazelhoseinzadeh@yahoo.com>
2024-05-14 21:33:18 +0000

    Add function to create random montage

--- app_pages/page_2_Leaves_Study.py
+++ app_pages/page_2_Leaves_Study.py
@@ -44,6 +44,53 @@ def page_2_Leaves_Study_content():
       labels = os.listdir(my_data_dir+ '/validation')
       label_to_display = st.selectbox(label="Select label", options=labels, index=0)
       if st.button("Create Montage"):
-        pass
+        image_montage(dir_path= my_data_dir + '/validation',
+                      label_to_display=label_to_display,
+                      nrows=8, ncols=3, figsize=(10,25))
       st.write("---")
 
+
+
+def image_montage(dir_path, label_to_display, nrows, ncols, figsize=(15,10)):
+  sns.set_style("ticks")
+  labels = os.listdir(dir_path)
+
+  # PowderyöMildew or Healthy Leaves
+  if label_to_display in labels:
+
+    # Number of total images in the folder
+    images_list = os.listdir(dir_path+'/'+ label_to_display)
+    if nrows * ncols < len(images_list):
+      img_idx = random.sample(images_list, nrows * ncols)
+    else:
+      print(
+          f"Decrease nrows or ncols to create your montage. \n"
+          f"There are {len(images_list)} in your subset. "
+          f"You requested a montage with {nrows * ncols} spaces")
+      return
+    
+
+    # create list of axes indices based on nrows and ncols
+    list_rows= range(0,nrows)
+    list_cols= range(0,ncols)
+    plot_idx = list(itertools.product(list_rows,list_cols))
+
+
+    # create a Figure and display images
+    fig, axes = plt.subplots(nrows=nrows,ncols=ncols, figsize=figsize)
+    for x in range(0,nrows*ncols):
+      img = imread(dir_path + '/' + label_to_display + '/' + img_idx[x])
+      img_shape = img.shape
+      axes[plot_idx[x][0], plot_idx[x][1]].imshow(img)
+      axes[plot_idx[x][0], plot_idx[x][1]].set_title(f"Width {img_shape[1]}px x Height {img_shape[0]}px")
+      axes[plot_idx[x][0], plot_idx[x][1]].set_xticks([])
+      axes[plot_idx[x][0], plot_idx[x][1]].set_yticks([])
+    plt.tight_layout()
+    
+    st.pyplot(fig=fig)
+    # plt.show()
+
+
+  else:
+    print("The label you selected doesn't exist.")
+    print(f"The existing options are: {labels}")
