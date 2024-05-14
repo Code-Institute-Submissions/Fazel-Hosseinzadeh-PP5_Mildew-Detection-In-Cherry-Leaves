2ab49cb6dd02136e0c9ef743797eab34798467eb
Fazel HosseinzadehDastak <fazelhoseinzadeh@yahoo.com>
2024-05-14 20:19:00 +0000

    Add  content structure of Quic Project Summary

--- app_pages/page_1_project_summary.py
+++ app_pages/page_1_project_summary.py
@@ -2,4 +2,29 @@ import streamlit as st
 
 
 def page_1_project_summary_content():
-    st.write("summary page")
+
+    st.write("### Quick Project Summary")
+    st.info(
+        f"""
+        **General Information**\n\n
+
+
+        \n\n
+        """)
+
+    st.warning(
+        f"""
+        **General Information**\n\n
+
+
+        \n\n
+        """)
+
+    st.success(
+        f"""
+        **General Information**\n\n
+
+
+        \n\n
+        """)
+     
