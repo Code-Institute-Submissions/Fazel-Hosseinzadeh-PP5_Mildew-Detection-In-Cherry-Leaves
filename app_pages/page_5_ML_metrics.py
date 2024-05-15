2faf8c965535cf41bfee9d503caa8b04d4505c48
Fazel-Hosseinzadeh <fazelhoseinzadeh@yahoo.com>
2024-05-15 16:23:36 +0200

    Add content, load labels distribution images

--- app_pages/page_5_ML_metrics.py
+++ app_pages/page_5_ML_metrics.py
@@ -1,5 +1,18 @@
 import streamlit as st
-
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib.image import imread
 
 def page_5_ML_metrics_content():
-    st.write("ML metrics page")
+    st.write(f"## **Machine Learning Metrics**")
+    st.write("---")
+    
+    st.info(f"### **Table of Labels Distribution per each dataset bin**")
+    
+    #Load saved image of lables distribution
+    version = 'v1'
+    labels_distribution = plt.imread(f"outputs/{version}/labels_distribution.png")
+    st.image(labels_distribution, caption='Labels Distribution on Train, Validation and Test Sets')
+    st.write("---")
+    
+    
