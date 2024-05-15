d7305a72e505fd41c9642e6a03e158bc40505c3f
Fazel-Hosseinzadeh <fazelhoseinzadeh@yahoo.com>
2024-05-15 13:00:15 +0200

    Add Content to Live Detector page

--- app_pages/page_3_Live_detector.py
+++ app_pages/page_3_Live_detector.py
@@ -1,5 +1,23 @@
 import streamlit as st
-
+import streamlit as st
+from PIL import Image
+import numpy as np
+import pandas as pd
 
 def page_3_Live_detector_content():
-    st.write("Live detector page")
+    
+    st.write(f"## **Live detector**")
+    st.write("---")
+    st.warning(f"Easily obtain instant analysis by uploading a photo of a cherry leaf to our machine learning model."
+            f"The analysis yields a brief report showcasing the probability of the leaf being infected or healthy."
+            f"This report is conveniently available for download in CSV format, ensuring quick access and"
+            f"integration into your workflow.")
+    st.write("---")
+    st.write(f"* [here](https://www.kaggle.com/datasets/codeinstitute/cherry-leaves) "
+               f"you can access **sample pictures** to test our ML model's performance." )
+    
+    images_buffer = st.file_uploader('Upload cherry leaves samples. You may select more than one.',
+                                        type='JPG',accept_multiple_files=True)
+    
+
+    
